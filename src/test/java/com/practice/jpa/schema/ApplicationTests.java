c3419bbac5aefee6f9cd5cd8f72711188971aa3e
ingduk2 <ingduk2@naver.com>
2022-06-12 16:04:36 +0900

    Initial commit

---
+++ src/test/java/com/practice/jpa/schema/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jpa.schema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
